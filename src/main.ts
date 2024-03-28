059ffe13b344511661f2ee0323cdb2899b9b8da9
Serhii Kaminskyi <kaminsky_s_s@ukr.net>
2024-03-29 01:05:06 +0200

    render listen

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
-  await app.listen(4200);
+  await app.listen(10000, '0.0.0.0');
 }
 bootstrap();
